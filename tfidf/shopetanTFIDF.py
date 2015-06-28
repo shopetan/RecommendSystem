9246a8d23b7e931cd332b1abaa71cd600da25880
shopetan <s.kikuchi1120@gmail.com>
2015-06-28 21:47:49 +0900

    ADD TFIDF

--- tfidf/shopetanTFIDF.py
+++ tfidf/shopetanTFIDF.py
@@ -2,6 +2,7 @@
 # coding:utf-8
 
 import MeCab,re
+import nltk
 
 def readDocument():
     f = open('documents.txt')
@@ -24,15 +25,42 @@ def getNounFromDocument(documents):
         node = mt.parseToNode(line)
         while node:
             if node.feature.split(",")[0] == "名詞":
-                replaceNode = re.sub( re.compile( "[!-/:-@[-`{-~]" ), "", node.surface ) 
+                replaceNode = re.sub( re.compile( "[!-/:-@[-`{-~]" ), "", node.surface )
                 if replaceNode != "" and replaceNode != " ":
+                    replaceNode = replaceNode.decode('utf-8')
                     noun[lineCount].append(replaceNode)
             node = node.next
         noun.append([])
         lineCount += 1
     return noun
 
+def writeTFIDFResultFromNouns(noun):
+    collection = nltk.TextCollection(noun)
+    uniqTerms = list(set(collection))
+    sortNoun = {}
+
+    f = open("descriptionNoun.txt","w")
+
+    for doc in noun:
+        try:
+            for term in uniqTerms:
+                sortNoun.update({term:collection.tf_idf(term, doc)})
+                # print "%s : %f" % (term, collection.tf_idf(term, doc))
+        except ZeroDivisionError:
+            print 'error!'
+        
+    wordCount = 0
+    for k, v in sorted(sortNoun.items(), key=lambda x:x[1],reverse=True):
+        if wordCount < 100:
+            if v > 0.0:
+                f.write(k.encode("utf-8"))
+                f.write(":")
+                f.write(str(v).encode("utf-8"))
+                f.write("\n")
+            wordCount += 1
+    f.close
+
 documents = readDocument()
 noun = getNounFromDocument(documents)
 
-print noun
+writeTFIDFResultFromNouns(noun)
